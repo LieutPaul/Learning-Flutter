83b242a1285dbb2becca077db339f4dc90a9fdbf
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-05-31 19:34:58 +0530

    Initial to do list done

--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -1,135 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:to_do_app/screens/home.dart';
 
 void main() {
-  runApp(MaterialApp(title: "To Do App", home: TaskList()));
+  runApp(const MyApp());
 }
 
-typedef TaskFunction = Function(Task task);
-
-class Task {
-  final String task;
-  bool completed;
-  Task(this.task, this.completed);
-
-  bool getCompleted() {
-    return completed;
-  }
-
-  void setCompleted(bool b) {
-    completed = b;
-  }
-}
-
-class TaskTile extends StatelessWidget {
-  final int index;
-  final Task task;
-  final TaskFunction changeTaskStatus;
-  const TaskTile(
-      {super.key,
-      required this.task,
-      required this.index,
-      required this.changeTaskStatus});
-
-  Color? _getColour(BuildContext context) {
-    if (task.completed == true) {
-      return const Color.fromARGB(255, 47, 150, 50);
-    } else {
-      return const Color.fromARGB(255, 227, 71, 60);
-    }
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-          backgroundColor: _getColour(context), child: Text('$index')),
-      title: Text(task.task),
-      onTap: () => changeTaskStatus(task),
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class TaskList extends StatefulWidget {
-  List<Task> tasks = [];
-  TaskList({super.key});
-
-  @override
-  State<TaskList> createState() => _TaskListState();
-}
-
-class _TaskListState extends State<TaskList> {
-  FloatingActionButton addANewTask() {
-    return FloatingActionButton(
-      child: const Icon(Icons.add),
-      onPressed: () => showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('Enter New Task'),
-          content: TextFormField(
-            controller: taskController,
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                taskController.clear();
-                Navigator.pop(context, 'Cancel');
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                _addTask(taskController.text);
-                taskController.clear();
-                Navigator.pop(context, 'OK');
-              },
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _addTask(String task) {
-    setState(() {
-      setState(() {
-        widget.tasks.add(Task(task, false));
-      });
-    });
-  }
-
-  void _changeTaskStatus(Task task) {
-    setState(() {
-      task.setCompleted(!(task.getCompleted()));
-    });
-  }
-
-  final TextEditingController taskController = TextEditingController();
-
-  @override
-  void dispose() {
-    taskController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text("To do List")),
-        body: Padding(
-            padding: const EdgeInsets.only(top: 10.0),
-            child: ListView(
-              children: widget.tasks.map((task) {
-                return TaskTile(
-                    task: task,
-                    index: widget.tasks.indexOf(task) + 1,
-                    changeTaskStatus: _changeTaskStatus);
-              }).toList(),
-            )),
-        floatingActionButton: addANewTask());
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return const MaterialApp(title: "ToDo App", home: Home());
   }
 }
---
+++ to_do_app/lib/screens/home.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/constants/constants.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: tdBGColor,
+        appBar: _buildAppBar(),
+        body: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Column(children: [
+              _searchBox(),
+              Expanded(
+                  child: ListView(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.only(top: 30, bottom: 30),
+                      child: const Text(
+                        "All ToDos",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w800),
+                      ))
+                ],
+              ))
+            ])));
+  }
+
+  Container _searchBox() {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(20)),
+        child: const TextField(
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(0),
+              prefixIcon: Icon(
+                Icons.search,
+                color: tdBlack,
+                size: 20,
+              ),
+              // https://api.flutter.dev/flutter/material/InputDecoration/prefixIconConstraints.html
+              prefixIconConstraints:
+                  BoxConstraints(maxHeight: 20, minHeight: 20),
+              border: InputBorder.none,
+              hintText: "Search"),
+        ));
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30,
+            ),
+            Icon(
+              Icons.person,
+              color: tdBlack,
+              size: 30,
+            )
+          ],
+        ));
+  }
+}
