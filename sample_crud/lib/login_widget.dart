e8198e38b89dc6870e620de8f6d931f147598339
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-06-04 15:06:14 +0530

    Sign Up Works

--- sample_crud/lib/login_widget.dart
+++ sample_crud/lib/login_widget.dart
@@ -1,10 +1,11 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:sample_crud/main.dart';
 
 class LoginWidget extends StatefulWidget {
-  final VoidCallback handleSignUpClick;
+  final VoidCallback handleSignUpClick; // To take user to the sign up page
   const LoginWidget({super.key, required this.handleSignUpClick});
 
   @override
@@ -12,6 +13,7 @@ class LoginWidget extends StatefulWidget {
 }
 
 class _LoginWidgetState extends State<LoginWidget> {
+  final loginformKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
@@ -23,6 +25,9 @@ class _LoginWidgetState extends State<LoginWidget> {
   }
 
   Future signIn() async {
+    if (!loginformKey.currentState!.validate()) {
+      return;
+    }
     showDialog(
         context: context,
         barrierDismissible: false,
@@ -36,6 +41,9 @@ class _LoginWidgetState extends State<LoginWidget> {
     } on FirebaseAuthException catch (e) {
       // ignore: avoid_print
       print(e);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(e.message!),
+      )); // To Display error messages
     }
     navigatorKey.currentState!.popUntil((route) => route.isFirst);
   }
@@ -44,66 +52,79 @@ class _LoginWidgetState extends State<LoginWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: ListView(children: [
-      Container(
-          margin: const EdgeInsets.only(top: 50),
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            children: [
-              const Center(
-                  child: Text(
-                "Provide Login Credentials",
-                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
-              )),
-              const SizedBox(height: 30),
-              TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
+      Form(
+        key: loginformKey,
+        child: Container(
+            margin: const EdgeInsets.only(top: 50),
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                const Center(
+                    child: Text(
+                  "Provide Login Credentials",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
+                )),
+                const SizedBox(height: 30),
+                TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (email) =>
+                        email != null && !EmailValidator.validate(email)
+                            ? 'Enter a valid Email'
+                            : null),
+                const SizedBox(height: 30),
+                TextFormField(
+                  obscureText: true,
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) => value != null && value.length < 6
+                      ? "Enter min 6 characters"
+                      : null,
                 ),
-              ),
-              const SizedBox(height: 30),
-              TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
+                const SizedBox(height: 20),
+                ElevatedButton.icon(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                  icon: const Icon(
+                    Icons.lock_open,
+                    size: 22,
+                    color: Colors.white,
+                  ),
+                  label: const Text(
+                    "Sign In",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: signIn,
                 ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                icon: const Icon(
-                  Icons.lock_open,
-                  size: 22,
-                  color: Colors.white,
+                const SizedBox(
+                  height: 20,
                 ),
-                label: const Text(
-                  "Sign In",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: signIn,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              RichText(
-                text: TextSpan(
-                    style: const TextStyle(color: Colors.black),
-                    text: "No Account? ",
-                    children: [
-                      TextSpan(
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = widget.handleSignUpClick,
-                          text: "Sign Up",
-                          style: const TextStyle(
-                              decoration: TextDecoration.underline,
-                              color: Color.fromARGB(255, 0, 100, 3)))
-                    ]),
-              )
-            ],
-          ))
+                RichText(
+                  text: TextSpan(
+                      style: const TextStyle(color: Colors.black),
+                      text: "No Account? ",
+                      children: [
+                        TextSpan(
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = widget
+                                  .handleSignUpClick, // To take to sign up page
+                            text: "Sign Up",
+                            style: const TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Color.fromARGB(255, 0, 100, 3)))
+                      ]),
+                )
+              ],
+            )),
+      )
     ]));
   }
 }
--- sample_crud/lib/signup_widget.dart
+++ sample_crud/lib/signup_widget.dart
@@ -1,10 +1,11 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:sample_crud/main.dart';
 
 class SignUpWidget extends StatefulWidget {
-  final VoidCallback handleLogInClick;
+  final VoidCallback handleLogInClick; // To take to login page
   const SignUpWidget({super.key, required this.handleLogInClick});
 
   @override
@@ -12,6 +13,9 @@ class SignUpWidget extends StatefulWidget {
 }
 
 class _SignUpWidgetState extends State<SignUpWidget> {
+  final formKey = GlobalKey<FormState>();
+  // Will help in verifying fields after user submits
+
   final emailController = TextEditingController();
   final password1Controller = TextEditingController();
   final password2Controller = TextEditingController();
@@ -25,6 +29,10 @@ class _SignUpWidgetState extends State<SignUpWidget> {
   }
 
   Future signUp() async {
+    if (!formKey.currentState!.validate()) {
+      return;
+    }
+
     showDialog(
         context: context,
         barrierDismissible: false,
@@ -37,6 +45,9 @@ class _SignUpWidgetState extends State<SignUpWidget> {
     } on FirebaseAuthException catch (e) {
       // ignore: avoid_print
       print(e);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(e.message!),
+      )); // To display error messages
     }
 
     navigatorKey.currentState!.popUntil((route) => route.isFirst);
@@ -47,76 +58,94 @@ class _SignUpWidgetState extends State<SignUpWidget> {
     return Scaffold(
         resizeToAvoidBottomInset: false,
         body: ListView(children: [
-          Container(
-              margin: const EdgeInsets.only(top: 50),
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                children: [
-                  const Center(
-                      child: Text(
-                    "Provide Sign-up Credentials",
-                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
-                  )),
-                  const SizedBox(height: 30),
-                  TextField(
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
+          Form(
+            key: formKey,
+            child: Container(
+                margin: const EdgeInsets.only(top: 50),
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  children: [
+                    const Center(
+                        child: Text(
+                      "Provide Sign-up Credentials",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
+                    )),
+                    const SizedBox(height: 30),
+                    TextFormField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Email',
+                        ),
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: (email) =>
+                            email != null && !EmailValidator.validate(email)
+                                ? 'Enter a valid Email'
+                                : null),
+                    const SizedBox(height: 30),
+                    TextFormField(
+                      obscureText: true,
+                      controller: password1Controller,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) => value != null && value.length < 6
+                          ? "Enter min 6 characters"
+                          : null,
                     ),
-                  ),
-                  const SizedBox(height: 30),
-                  TextField(
-                    obscureText: true,
-                    controller: password1Controller,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
+                    const SizedBox(height: 30),
+                    TextFormField(
+                      obscureText: true,
+                      controller: password2Controller,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Confirm Password',
+                      ),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) =>
+                          value != null && value != password1Controller.text
+                              ? "Passwords don't match"
+                              : null,
                     ),
-                  ),
-                  const SizedBox(height: 30),
-                  TextField(
-                    obscureText: true,
-                    controller: password2Controller,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Confirm Password',
+                    const SizedBox(height: 20),
+                    ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black),
+                      icon: const Icon(
+                        Icons.lock_open,
+                        size: 22,
+                        color: Colors.white,
+                      ),
+                      label: const Text(
+                        "Sign Up",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: signUp,
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    style:
-                        ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                    icon: const Icon(
-                      Icons.lock_open,
-                      size: 22,
-                      color: Colors.white,
+                    const SizedBox(
+                      height: 20,
                     ),
-                    label: const Text(
-                      "Sign Up",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: signUp,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  RichText(
-                    text: TextSpan(
-                        style: const TextStyle(color: Colors.black),
-                        text: "Already have an Account? ",
-                        children: [
-                          TextSpan(
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = widget.handleLogInClick,
-                              text: "Log In",
-                              style: const TextStyle(
-                                  decoration: TextDecoration.underline,
-                                  color: Color.fromARGB(255, 0, 100, 3)))
-                        ]),
-                  )
-                ],
-              ))
+                    RichText(
+                      text: TextSpan(
+                          style: const TextStyle(color: Colors.black),
+                          text: "Already have an Account? ",
+                          children: [
+                            TextSpan(
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = widget
+                                      .handleLogInClick, // To take to login page
+                                text: "Log In",
+                                style: const TextStyle(
+                                    decoration: TextDecoration.underline,
+                                    color: Color.fromARGB(255, 0, 100, 3)))
+                          ]),
+                    )
+                  ],
+                )),
+          )
         ]));
   }
 }
